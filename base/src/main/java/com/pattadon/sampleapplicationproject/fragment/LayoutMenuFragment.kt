9638221c995be47cc4a7958d2a40bb5d25a2b9d7
pattadon <pattadon@23perspective.com>
2017-08-18 17:55:56 +0700

    update code

--- base/src/main/java/com/pattadon/sampleapplicationproject/fragment/LayoutMenuFragment.kt
+++ base/src/main/java/com/pattadon/sampleapplicationproject/fragment/LayoutMenuFragment.kt
@@ -8,7 +8,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.pattadon.sampleapplicationproject.R
-import kotlinx.android.synthetic.main.fragment_network_menu.*
+import kotlinx.android.synthetic.main.fragment_layout_menu.*
 
 /**
  * Created by pattadon on 8/18/17.
@@ -18,23 +18,31 @@ class LayoutMenuFragment : Fragment() {
 
     companion object {
         @JvmStatic
-        fun newInstance(): NetworkMenuFragment {
-            val networkFragment = NetworkMenuFragment()
+        fun newInstance(): LayoutMenuFragment {
+            val layoutMenuFragment = LayoutMenuFragment()
             val args = Bundle()
-            networkFragment.arguments = args
-            return networkFragment
+            layoutMenuFragment.arguments = args
+            return layoutMenuFragment
         }
     }
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater?.inflate(R.layout.fragment_network_menu, container, false);
+        return inflater?.inflate(R.layout.fragment_layout_menu, container, false);
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        retrofit_btn.setOnClickListener {
+        layout_menu_basic_btn.setOnClickListener {
             val intent = Intent(Intent.ACTION_VIEW,
-                    Uri.parse("https://pattadon.com/samplenetwork"))
+                    Uri.parse("https://pattadon.com/basiclayout"))
+            intent.`package` = activity.packageName
+            intent.addCategory(Intent.CATEGORY_BROWSABLE)
+            startActivity(intent)
+        }
+
+        layout_menu_support_library_btn.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW,
+                    Uri.parse("https://pattadon.com/supportlayout"))
             intent.`package` = activity.packageName
             intent.addCategory(Intent.CATEGORY_BROWSABLE)
             startActivity(intent)
---
+++ supportlayout/src/main/java/com/pattadon/supportlayout/SupportLibrarySampleActivity.kt
@@ -0,0 +1,12 @@
+package com.pattadon.supportlayout
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class SupportLibrarySampleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_support_library_sample)
+    }
+}
